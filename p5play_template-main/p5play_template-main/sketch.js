ed9e2bee809ab7fe8b72f833609a51f042015d97
Samridhi-Sinha2010 <115167581+Samridhi-Sinha2010@users.noreply.github.com>
2022-10-06 19:53:23 +0530

    Add files via upload

---
+++ p5play_template-main/p5play_template-main/sketch.js
@@ -0,0 +1,28 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,50,30);
+}
+
+function draw() 
+{
+  background(30);
+ drawSprites();
+ 
+ if(keyDown("right")){
+  box.x = box.x + 2;
+}
+if(keyDown("left")){
+  box.x = box.x - 2;
+}
+if(keyDown("up")){
+  box.y = box.y - 2;
+}
+if(keyDown("down")){
+  box.y = box.y + 2;
+}
+}
+
+
+
+
